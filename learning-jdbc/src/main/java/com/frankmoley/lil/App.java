b6cda769d0b7b33bc03456fbb17c171a1283105b
Josh Sivertson <130604508+j-sivertson@users.noreply.github.com>
2025-09-05 18:05:27 +0000

    Update: Connected the the DB and get all data from services

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -1,10 +1,16 @@
 package com.frankmoley.lil;
 
-/**
- * Hello world!
- */
+import java.util.List;
+
+import com.frankmoley.lil.data.dao.ServiceDao;
+import com.frankmoley.lil.data.entity.Service;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ServiceDao serviceDao = new ServiceDao();
+        List<Service> services = serviceDao.getAll();
+        System.out.println("**** SERVICES ****");
+        System.out.println("\n**** OUTPUT *****");
+        services.forEach(System.out::println);
     }
 }
