909a0307b842d7f22bc07560a63acacb4f0a8e2c
Josh Sivertson <130604508+j-sivertson@users.noreply.github.com>
2025-09-05 19:46:03 +0000

    Update: Added the create function

--- learning-jdbc/src/main/java/com/frankmoley/lil/App.java
+++ learning-jdbc/src/main/java/com/frankmoley/lil/App.java
@@ -1,5 +1,6 @@
 package com.frankmoley.lil;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,5 +19,11 @@ public class App {
         services.forEach(System.out::println);
         Optional<Service> service = serviceDao.getOne(services.get(0).getServiceId());
         System.out.println("\n*** GET ONE ***\n" + service.get());
+        Service newService = new Service();
+        newService.setName("fooBarBazz" + System.currentTimeMillis());
+        newService.setPrice(new BigDecimal(4.35));
+        newService = serviceDao.create(newService);
+        System.out.println("\n*** CREATE ***\n" + newService);
+
     }
 }
